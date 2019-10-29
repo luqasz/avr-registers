1850ca3cc4d4743bfba9374e8798c7b7a0f34e45
Łukasz Kostka <lukasz.kostka@netng.pl>
2019-10-29 13:36:58 +0100

    Filter devices without datasheet

--- gen_registers.py
+++ gen_registers.py
@@ -3,12 +3,12 @@
 from pathlib import Path
 import yaml
 
-from parser import parseString
+from parser import parseFile
 
 
 def parse():
     for dev_file in Path('./atdf').glob('*.atdf'):
-        yield from parseString(str(dev_file.resolve()))
+        yield from parseFile(str(dev_file.resolve()))
 
 
 def bit_field(field):
@@ -29,15 +29,29 @@ def register(reg):
 
 
 def generate():
-    for device in set(parse()):
+    for device in parse():
         yml = dict()
         yml['name'] = device['name'].lower()
         yml['registers'] = list(register(reg) for reg in device.registers)
         yield yml
 
 
+def filterDevices(yml):
+    # No datasheet available
+    return yml['name'] not in (
+        'atmega64hve2',
+        'atmega8hva',
+        'atmega16hva',
+        'atmega16hvb',
+        'atmega16hvbrevb',
+        'atmega32hvb',
+        'atmega32hvbrevb',
+        'atmega64hve2',
+    )
+
+
 if __name__ == '__main__':
-    for device in generate():
+    for device in filter(filterDevices, generate()):
         dst = Path('./registers') / (device['name'].lower() + '.yml')
         dst.touch()
         with dst.open('wt') as dst:
