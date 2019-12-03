a89de26a85d16980ad56531c5749c178367ddcf3
Łukasz Kostka <lukasz.kostka@netng.pl>
2019-12-03 20:37:56 +0100

    Remove GPIOR bit fields

--- tests/conftest.py
+++ tests/conftest.py
@@ -22,111 +22,92 @@ def id_device_reg(param):
     return dev_name
 
 
-all_devices = tuple(dict(name=name, registers=regs) for name,regs in parsed())
+all_devices = tuple(dict(name=name, registers=regs) for name, regs in parsed())
 
 
 @pytest.fixture(
-        ids=lambda x: x['name'],
-        params=all_devices,
-    )
+    ids=lambda x: x['name'],
+    params=all_devices,
+)
 def device(request):
     yield request.param
 
 
-@pytest.fixture(
-        ids=id_device_reg,
-        params=device_reg(
-            devices=all_devices,
-            needle=lambda x: x.startswith('OCDR'),
-            )
-        )
+@pytest.fixture(ids=id_device_reg, params=device_reg(
+    devices=all_devices,
+    needle=lambda x: x.startswith('OCDR'),
+))
 def OCDR(request):
     return request.param
 
 
-@pytest.fixture(
-        ids=id_device_reg,
-        params=device_reg(
-            devices=all_devices,
-            needle=lambda x: x == 'TCCR1A'
-            )
-        )
+@pytest.fixture(ids=id_device_reg, params=device_reg(
+    devices=all_devices,
+    needle=lambda x: x.startswith('GPIOR'),
+))
+def GPIOR(request):
+    return request.param
+
+
+@pytest.fixture(ids=id_device_reg, params=device_reg(
+    devices=all_devices,
+    needle=lambda x: x == 'TCCR1A',
+))
 def TCCR1A(request):
     return request.param
 
 
-@pytest.fixture(
-        ids=id_device_reg,
-        params=device_reg(
-            devices=all_devices,
-            needle=lambda x: x == 'TCCR1B'
-            )
-        )
+@pytest.fixture(ids=id_device_reg, params=device_reg(
+    devices=all_devices,
+    needle=lambda x: x == 'TCCR1B',
+))
 def TCCR1B(request):
     return request.param
 
 
-@pytest.fixture(
-        ids=id_device_reg,
-        params=device_reg(
-            devices=all_devices,
-            needle=lambda x: x == 'TCCR3A'
-            )
-        )
+@pytest.fixture(ids=id_device_reg, params=device_reg(
+    devices=all_devices,
+    needle=lambda x: x == 'TCCR3A',
+))
 def TCCR3A(request):
     return request.param
 
 
-@pytest.fixture(
-        ids=id_device_reg,
-        params=device_reg(
-            devices=all_devices,
-            needle=lambda x: x == 'TCCR3B'
-            )
-        )
+@pytest.fixture(ids=id_device_reg, params=device_reg(
+    devices=all_devices,
+    needle=lambda x: x == 'TCCR3B',
+))
 def TCCR3B(request):
     return request.param
 
 
-@pytest.fixture(
-        ids=id_device_reg,
-        params=device_reg(
-            devices=all_devices,
-            needle=lambda x: x == 'TCCR4A'
-            )
-        )
+@pytest.fixture(ids=id_device_reg, params=device_reg(
+    devices=all_devices,
+    needle=lambda x: x == 'TCCR4A',
+))
 def TCCR4A(request):
     return request.param
 
 
-@pytest.fixture(
-        ids=id_device_reg,
-        params=device_reg(
-            devices=all_devices,
-            needle=lambda x: x == 'TCCR4B'
-            )
-        )
+@pytest.fixture(ids=id_device_reg, params=device_reg(
+    devices=all_devices,
+    needle=lambda x: x == 'TCCR4B',
+))
 def TCCR4B(request):
     return request.param
 
 
-@pytest.fixture(
-        ids=id_device_reg,
-        params=device_reg(
-            devices=all_devices,
-            needle=lambda x: x == 'TCCR5A'
-            )
-        )
+@pytest.fixture(ids=id_device_reg, params=device_reg(
+    devices=all_devices,
+    needle=lambda x: x == 'TCCR5A',
+))
 def TCCR5A(request):
     return request.param
 
 
-@pytest.fixture(
-        ids=id_device_reg,
-        params=device_reg(
-            devices=all_devices,
-            needle=lambda x: x == 'TCCR5B'
-            )
-        )
+@pytest.fixture(ids=id_device_reg, params=device_reg(
+    devices=all_devices,
+    needle=lambda x: x == 'TCCR5B',
+))
 def TCCR5B(request):
     return request.param
--- tests/test_registers.py
+++ tests/test_registers.py
@@ -1,6 +1,13 @@
 import pytest
 
+
 def test_OCDR(OCDR):
     """OCDR is a data register without bit fields."""
     _, reg = OCDR
+    assert reg['bit_fields'] == []
+
+
+def test_GPIOR(GPIOR):
+    """GPIOR is a data register without bit fields."""
+    _, reg = GPIOR
     assert reg['bit_fields'] == []
